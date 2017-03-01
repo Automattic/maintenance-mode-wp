b276f076a0ed26cf1869890fd708827bf36b0ecf
Benoît Chantre <mail@benoitchantre.com>
2017-03-01 22:34:21 +0100

    i18n

--- template-maintenance-mode.php
+++ template-maintenance-mode.php
@@ -4,8 +4,8 @@
 	<?php wp_head(); // allow for remote-login on mapped domains ?>
 </head>
 <body>
-	<h1>Howdy!</h1>
-	<p>We're just freshening things up a bit; back in a few!</p>
+	<h1><?php esc_html_e( 'Howdy!', 'maintenance-mode' ); ?></h1>
+	<p><?php esc_html_e( "We're just freshening things up a bit; back in a few!", 'maintenance-mode' ); ?></p>
 	<?php wp_footer(); ?>
 </body>
 </html>
