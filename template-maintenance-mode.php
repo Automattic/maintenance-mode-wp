a71f8c015d64b6aefcff826f8c9625274021bd0e
Benoît Chantre <mail@benoitchantre.com>
2017-09-24 13:24:31 +0200

    Use a more formal message

--- template-maintenance-mode.php
+++ template-maintenance-mode.php
@@ -4,9 +4,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<?php wp_head(); // allow for remote-login on mapped domains ?>
 </head>
-<body>
-	<h1><?php esc_html_e( 'Howdy!', 'maintenance-mode' ); ?></h1>
-	<p><?php esc_html_e( 'We\'re just freshening things up a bit; back in a few!', 'maintenance-mode' ); ?></p>
+<body class="maintenance-mode">
+	<h1><?php esc_html_e( 'Sorry, we\'re down for&nbsp;maintenance.', 'maintenance-mode' ); ?></h1>
+	<p><?php esc_html_e( 'We\'ll be back up shortly.', 'maintenance-mode' ); ?></p>
 	<?php wp_footer(); ?>
 </body>
 </html>
