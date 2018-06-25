8ed99122ca74ba382de2c3b3560657d31a841d6f
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2018-06-25 11:32:49 -0600

    Change REST error message to be more descriptive and have a more accu… (#31)
    * Change REST error message to be more descriptive and have a more accurate error code
    
    Fixes issue #20
    Error code from 401 to 503
    
    A slight change to the WP_Error name to be more descriptive..
    
    `$rest_error` was too general, so I think let's keep `$unauthorized_error`
    
    We'll use `vip_maintenance_mode_unauthorized` as the WP_Error name
    
    * Changed WP_Error name to be more in line with the error code 503...and as well, the variable name since we're moving away from the term unau

--- maintenance-mode.php
+++ maintenance-mode.php
@@ -116,21 +116,21 @@ function vip_maintenance_mode_restrict_rest_api( $result ) {
 		return $result;
 	}
 
-	$error_message = apply_filters( 'vip_maintenance_mode_rest_api_error_message', __( 'REST API access is currently restricted while we refresh things a bit.', 'maintenance-mode' ) );
-	$unauthorized_error = new WP_Error(
-		'vip_maintenance_mode_rest_not_logged_in',
+	$error_message = apply_filters( 'vip_maintenance_mode_rest_api_error_message', __( 'REST API access is currently restricted while this site is undergoing maintenance.', 'maintenance-mode' ) );
+	$maintenace_rest_error = new WP_Error(
+		'vip_maintenance_mode_rest_error',
 		$error_message,
 		array(
-			'status' => 401,
+			'status' => 503,
 		)
 	);
 
 	if ( ! is_user_logged_in() ) {
-		return $unauthorized_error;
+		return $maintenace_rest_error;
 	}
 
 	if ( ! current_user_can_bypass_vip_maintenance_mode() ) {
-		return $unauthorized_error;
+		return $maintenace_rest_error;
 	}
 
 	return $result;
